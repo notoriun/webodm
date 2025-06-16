da5dac58e1c44c449bbf014b75b816fe1473c4ab
Ygor Cruz Felix <ygorcfelix@gmail.com>
2025-06-16 17:09:30 -0300

    Espaco opcional

--- app/auth/querystring_jwt_token.py
+++ app/auth/querystring_jwt_token.py
@@ -6,5 +6,5 @@ class QueryStringJWTAuthentication(JWTAuthentication):
         raw_token = request.query_params.get("token")
         if raw_token is None:
             return None
-        validated_token = self.get_validated_token(raw_token.replace("Bearer ", ""))
+        validated_token = self.get_validated_token(raw_token.replace(r"Bearer\s?", ""))
         return self.get_user(validated_token), validated_token
