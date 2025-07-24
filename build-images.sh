0617ce5158da3ed647510804ba79edada9854d8e
Ygor Felix <ygorcfelix@gmail.com>
2025-07-23 23:27:32 -0300

    fix: corrigido script de build

--- build-images.sh
+++ build-images.sh
@@ -3,19 +3,22 @@
 set -e
 
 DOCKERFILE="Dockerfile.web"
+BASE_IMAGE_VERSION="1.0.1"
 
 usage() {
-  echo "Usage: $0 -t <image_tag> [-f <dockerfile>]"
+  echo "Usage: $0 -t <image_tag> [-f <dockerfile>] [-b <base_image_version>]"
   echo ""
   echo "  -t <image_tag>     Required. Full image tag (e.g., user/app:latest)"
   echo "  -f <dockerfile>    Optional. Dockerfile to use (default: Dockerfile.web)"
+  echo "  -b <base_image_version>    Optional. Base image version to use on build (default: 1.0.1)"
   exit 1
 }
 
-while getopts "t:f:" opt; do
+while getopts "t:f:b:" opt; do
   case $opt in
     t) TAG="$OPTARG" ;;
     f) DOCKERFILE="$OPTARG" ;;
+    b) BASE_IMAGE_VERSION="$OPTARG" ;;
     *) usage ;;
   esac
 done
@@ -34,10 +37,10 @@ echo "  🍏 ARM64 -> $TAG_ARM64"
 echo ""
 
 echo "🔨 Building amd64..."
-podman build --arch amd64 -f "$DOCKERFILE" -t "$TAG_AMD64" .
+podman build --arch amd64 -f "$DOCKERFILE" -t "$TAG_AMD64" --build-arg VER_PGI_INFRA=$BASE_IMAGE_VERSION-amd64  .
 
 echo "🔨 Building arm64..."
-podman build --arch arm64 -f "$DOCKERFILE" -t "$TAG_ARM64" .
+podman build --arch arm64 -f "$DOCKERFILE" -t "$TAG_ARM64" --build-arg VER_PGI_INFRA=$BASE_IMAGE_VERSION-arm64 .
 
 echo "🚀 Pushing amd64..."
 podman push "$TAG_AMD64"
